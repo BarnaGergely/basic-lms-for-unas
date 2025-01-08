3f7801ef4619d14b1cd6444ace89e5f4cc13b784
Barna Gergely <h144988@stud.u-szeged.hu>
2025-01-08 16:49:06 +0100

    base

---
+++ basic-lms-for-unas.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Basic LMS with UNAS
+ * Plugin URI:      https://TODO:
+ * Description:     Basic LMS with automatic user management from UNAS
+ * Author:          GergelyBarna
+ * Author URI:      https://mediterranfarm.hu
+ * Text Domain:     basic-lms-for-unas
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Basic_Lms_For_Unas
+ */
+
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
